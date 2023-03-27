7c084104de74c34445ac511ac8be260e3d7907c7
gkumar-pandey <gautamshekhar078@gmail.com>
2023-03-28 03:22:45 +0530

    style: fix size of footer

--- components/homepage/components/common/footer/footer.component.tsx
+++ components/homepage/components/common/footer/footer.component.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image"
 import Link from "next/link"
 import {
     IconBackToTop,
-    FbIconSvg,
-    instagramIconSvg,
-    twitterIconSvg,
-    linkedinIconSvg,
+    FacebookIcon,
+    InstagramIcon,
+    TwitterIcon,
+    LinkedinIcon,
     FooterLogo,
 } from "@/public"
 import { Container } from "../../container/container.component"
@@ -13,19 +12,58 @@ import { SocialIcon } from "../../primitive/social-icon/social-icon.component"
 import { Button } from "../../primitive/button/button.component"
 import { FooterLink } from "../../primitive/footerlink/footerlink.component"
 
+
+const FooterLinkTittle = ({ title }: { title: string }) => <>
+    <h2 className=" text-[#CCD0D5] font-sans text-lg ">
+        {title}
+    </h2>
+</>
+
+const FooterLinkList = ({ title, link }: { title: string, link: string, }) => {
+
+    const handleSmoothScroll = () => {
+        const ScrollSection = document.querySelector(`${link}`);
+        ScrollSection?.scrollIntoView({ behavior: "smooth" })
+
+    }
+
+    if (link[0] == '#') {
+        return <>
+            <FooterLink type="section" >
+                <p onClick={handleSmoothScroll} >{title}</p>
+            </FooterLink>
+        </>
+    }
+
+    return <>
+        <FooterLink type="external" >
+            <Link
+                href={link}
+                target="_blank"
+
+            >
+                {title}
+            </Link>
+        </FooterLink></>
+}
+
+
 export const Footer = () => {
     const socialIconData = [
-        { icon: FbIconSvg, link: "https://www.facebook.com/zippycrowd/" },
-        { icon: twitterIconSvg, link: "https://twitter.com/zippy_crowd" },
+        { icon: FacebookIcon, link: "https://www.facebook.com/zippycrowd/" },
+        { icon: TwitterIcon, link: "https://twitter.com/zippy_crowd" },
         {
-            icon: linkedinIconSvg,
+            icon: LinkedinIcon,
             link: "https://www.linkedin.com/company/6643469/",
         },
         {
-            icon: instagramIconSvg,
+            icon: InstagramIcon,
             link: "https://www.instagram.com/zippycrowd/",
         },
     ]
+
+
+
 
     const handleBackToTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth" })
@@ -33,143 +71,44 @@ export const Footer = () => {
 
     return (
         <section className="pt-10  footer-header footer" id="footer-section">
-            <div className="">
+            <div className="footer-bg-img" >
                 <Container>
-                    <div className=" footer-container ">
-                        <div>
-                            <div className="px-3">
-                                <Image src={FooterLogo} alt="footer-logo" />
-                            </div>
+                    <div className="footer-container pb-5 " >
+                        <div >
+                            <img src={FooterLogo} alt="footer-logo" className=" md:w-2/5 sm:w-9/12 " />
                         </div>
-                        <div className=" my-10">
-                            <div className="flex gap-5 font-sans lg:flex-wrap xl:flex-wrap sm:flex-wrap  2xl:flex-wrap  ">
-                                <div>
-                                    <div className="px-3 text-lg text-[#fff]">
-                                        <h2 className="p-2 text-[#CCD0D5] font-sans text-lg ">
-                                            Company
-                                        </h2>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/about-us/"
-                                                }
-                                                target="_blank"
-                                            >
-                                                About us
-                                            </Link>
-                                        </FooterLink>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/work-with-us/"
-                                                }
-                                                target="_blank"
-                                            >
-                                                Work with us
-                                            </Link>
-                                        </FooterLink>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/contact-us/"
-                                                }
-                                                target="_blank"
-                                            >
-                                                Contact us
-                                            </Link>
-                                        </FooterLink>
-                                    </div>
+                        <div className="my-12">
+                            <div className="grid gap-y-10 2xl:w-1/2  2xl:grid-cols-3 sm:grid-cols-2 grid-cols-4">
+                                <div className="text-lg text-[#fff]">
+                                    <FooterLinkTittle title="Company" />
+                                    <FooterLinkList title="About us" link='https://blog.zippycrowd.com/about-us/' />
+                                    <FooterLinkList title="Work with us" link="https://blog.zippycrowd.com/work-with-us/" />
+                                    <FooterLinkList title="Contact us" link="https://blog.zippycrowd.com/contact-us/ " />
                                 </div>
-                                <div>
-                                    <div className="px-3 text-lg text-[#fff]">
-                                        <h2 className="p-2 text-[#CCD0D5] font-sans text-lg ">
-                                            Resources
-                                        </h2>
-                                        <p className="p-2">FAQs</p>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/"
-                                                }
-                                                target="_blank"
-                                            >
-                                                Community
-                                            </Link>
-                                        </FooterLink>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/"
-                                                }
-                                                target="_blank"
-                                            >
-                                                Blog
-                                            </Link>
-                                        </FooterLink>
-                                    </div>
+                                <div className=" text-lg text-[#fff]">
+                                    <FooterLinkTittle title="Resources" />
+                                    <FooterLinkList title="FAQs" link="#faq-section" />
+                                    <FooterLinkList title="Community" link="https://blog.zippycrowd.com/" />
+                                    <FooterLinkList title="Blog" link="https://blog.zippycrowd.com/" />
                                 </div>
-                                <div>
-                                    <div className="px-3  text-lg text-[#fff]">
-                                        <h2 className="p-2 text-[#CCD0D5] font-sans text-lg ">
-                                            Legal
-                                        </h2>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/legal-hub/gdpr-addendum/"
-                                                }
-                                                target="_blank"
-                                            >
-                                                GDPR
-                                            </Link>
-                                        </FooterLink>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/legal-hub/"
-                                                }
-                                                target="_blank"
-                                            >
-                                                Security
-                                            </Link>
-                                        </FooterLink>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/legal-hub/privacy-policy/"
-                                                }
-                                                target="_blank"
-                                            >
-                                                Privacy
-                                            </Link>
-                                        </FooterLink>
-                                    </div>
+                                <div className=" text-lg text-[#fff]">
+                                    <FooterLinkTittle title="Legal" />
+                                    <FooterLinkList title="GDPR" link="https://blog.zippycrowd.com/legal-hub/gdpr-addendum/" />
+                                    <FooterLinkList title="Security" link="https://blog.zippycrowd.com/legal-hub/" />
+                                    <FooterLinkList title="Privacy" link="https://blog.zippycrowd.com/legal-hub/privacy-policy/" />
                                 </div>
-
                                 <div className="lg:w-full xl:w-full 2xl:w-full ">
-                                    <div className="px-3 text-lg font-sans text-[#fff]">
-                                        <h2 className="p-2 text-[#ccd0d5] font-sans text-lg ">
-                                            Platform
-                                        </h2>
-                                        <p className="p-2 text-base">
-                                            Features
-                                        </p>
-                                        <FooterLink>
-                                            <Link
-                                                href={
-                                                    "https://blog.zippycrowd.com/product/getting-started/"
-                                                }
-                                            >
-                                                Getting Started
-                                            </Link>
-                                        </FooterLink>
+                                    <div className="text-lg font-sans text-[#fff]">
+                                        <FooterLinkTittle title="Platform" />
+                                        <FooterLinkList title="Features" link="#how-zippy-work" />
+                                        <FooterLinkList title="Getting Started" link="https://blog.zippycrowd.com/product/getting-started/" />
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="flex my-5 items-center justify-between sm:flex-wrap ">
-                            <div className="flex gap-5 sm:justify-start sm:w-full">
+                        <div className="grid grid-cols-2 sm:grid-cols-1 sm:gap-y-10 ">
+                            <div className="flex gap-10">
                                 <Link href={"https://app.zippycrowd.com/login"}>
                                     <Button variant="filled">
                                         Get Started
@@ -179,12 +118,12 @@ export const Footer = () => {
                                     <Button variant="outlined">Login</Button>
                                 </Link>
                             </div>
-                            <div>
+                            <div className="flex justify-end sm:justify-start " >
                                 <div
-                                    className="flex mx-3 items-center justify-center text-[#fff] font-sans sm:py-[1rem]  "
+                                    className="flex items-center gap-2 cursor-pointer "
                                     onClick={handleBackToTop}
                                 >
-                                    <Image
+                                    <img
                                         src={IconBackToTop}
                                         alt="Back To top icon"
                                     />
@@ -192,11 +131,11 @@ export const Footer = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex py-[2rem] items-center w-full  sm:flex-wrap-reverse  ">
-                            <div className=" sm:w-full text-center py-5">
+                        <div className="flex items-center w-full justify-between  sm:flex-wrap-reverse  ">
+                            <div className="sm:w-full py-5">
                                 <p>© Copyright 2022. All rights reserved</p>
                             </div>
-                            <div className=" w-2/6 sm:w-full justify-evenly flex gap-3  sm:justify-start">
+                            <div className=" w-2/6  md:justify-start 2xl:w-4/6 2xl:justify-start 2xl:pl-4 sm:w-full justify-evenly flex gap-3  sm:justify-start">
                                 <button>
                                     <Link
                                         href={
@@ -219,7 +158,7 @@ export const Footer = () => {
                                 </button>
                             </div>
 
-                            <div className="flex justify-end w-3/6 sm:my-[4rem] sm:w-full  sm:justify-center  ">
+                            <div className=" flex justify-end  sm:my-[4rem] sm:w-full  sm:justify-center  ">
                                 <div className="flex items-center justify-between">
                                     {socialIconData.map(
                                         ({ icon, link }, idx) => (
