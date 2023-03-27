b0bb9e96dc07909105246cef440022949432f1f7
gkumar-pandey <gautamshekhar078@gmail.com>
2023-03-28 03:25:16 +0530

    style: add hover effect on btn

--- components/homepage/components/primitive/button/button.component.style.ts
+++ components/homepage/components/primitive/button/button.component.style.ts
@@ -1,6 +1,6 @@
 const outlineClasses = ` py-[8px] border-2 border-[#85FAD7] font-sans px-[24px] text-[#85FAD7] font-semibold rounded text-lg  hover:bg-[#85FAD7] hover:text-[#000]`
 
-const filledClasses = `py-2.5 font-sans px-6 bg-[#85FAD7] hover:bg-[#61F0CE] text-[#000] font-semibold rounded text-lg `
+const filledClasses = `py-2.5 font-sans px-6 bg-[#85FAD7] hover:bg-[#61F0CE] text-[#000] font-semibold rounded text-lg hover:transform hover:-translate-y-0.5 hover:shadow-xl `
 
 const tagClasses = `border-2 p-2 m-2 rounded-[80px] bg-[#fff] font-sans font-semibold border-[#e188af]`
 
