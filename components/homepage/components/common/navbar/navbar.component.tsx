f88bdfcaebc4e7658b500a757c34fdf0b85a0531
gkumar-pandey <gautamshekhar078@gmail.com>
2023-04-02 19:14:37 +0530

    style: make navbar responsive

--- components/homepage/components/common/navbar/navbar.component.tsx
+++ components/homepage/components/common/navbar/navbar.component.tsx
@@ -64,7 +64,7 @@ export const Navbar = () => {
                                     scrollToSection("how-zippy-work")
                                 }
                             >
-                                How Zippy Works
+                                How Zippy works
                             </p>
                         </NavLink>
                         <NavLink>
