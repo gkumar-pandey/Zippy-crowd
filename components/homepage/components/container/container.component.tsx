65a1eb0bd9756520eb75264c653578f76b2ec1e5
gkumar-pandey <gautamshekhar078@gmail.com>
2023-04-02 19:15:03 +0530

    style: make container responsive

--- components/homepage/components/container/container.component.tsx
+++ components/homepage/components/container/container.component.tsx
@@ -5,5 +5,5 @@ interface ContainerProps {
 export const Container = (props: ContainerProps) => {
     const { children } = props
 
-    return <div className="max-w-[1140px] px-4  mx-auto my-0"> {children}</div>
+    return <div className="max-w-[1140px] px-[15px] mx-auto my-0"> {children}</div>
 }
