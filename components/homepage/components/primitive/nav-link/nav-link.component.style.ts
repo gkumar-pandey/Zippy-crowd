bcbe4a36ae6f36dfbfba40cfe470de1fd7df25e2
gkumar-pandey <gautamshekhar078@gmail.com>
2023-04-02 19:16:08 +0530

    update

--- components/homepage/components/primitive/nav-link/nav-link.component.style.ts
+++ components/homepage/components/primitive/nav-link/nav-link.component.style.ts
@@ -1,1 +1,1 @@
-export const navLinkStyle = `text-base cursor-pointer font-normal hover:text-[#B3BAE8] `
+export const navLinkStyle = `text-base cursor-pointer px-[5px] font-normal hover:text-[#B3BAE8] `
