b8180bf0341e0e6aecb74757aa84b69449834667
gkumar-pandey <gautamshekhar078@gmail.com>
2023-04-08 04:03:37 +0530

    style: fix the style

--- components/homepage/sections/sign-up-free/sign-up-free.section.tsx
+++ components/homepage/sections/sign-up-free/sign-up-free.section.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-
 import Link from "next/link"
 import { Container } from "../../components/container/container.component"
 import { Wave2Top } from "../../components/wave/wave-two-svg/wave-two-top.component"
@@ -32,59 +31,53 @@ export const SignUpSection = () => {
                 <Wave2Top />
             </div>
 
-            <div className="py-10">
+            <div className="py-10 ">
                 <Container>
-                    <div>
-                        <div className=" flex lg:flex-wrap-reverse md:flex-wrap-reverse sm:flex-wrap-reverse ">
-                            <div className="px-3 md:w-4/5   md:mx-auto   w-full lg:w-3/4 lg:mx-auto py-3  ">
+                    <div className="flex items-center lg:flex-wrap-reverse md:flex-wrap-reverse sm:flex-wrap-reverse ">
+                        <div className=" ml-[100px] lg:pl-[26px]  md:w-4/5 md:mx-auto w-full lg:w-3/4 lg:mx-auto sm:m-0 ">
+                            <h1 className="xl:text-3xl 2xl:text-3xl text-[28px]  mb-2  w-10/12 md:w-11/12 sm:text-2xl font-semibold font-sans text-[#fff] leading-[3rem] sm:w-full ">
+                                <span className="text-[#85FAD7]">
+                                    Build business{" "}
+                                </span>
+                                on the{" "}
+                                <span className="text-[#85FAD7]">
+                                    strength{" "}
+                                </span>{" "}
+                                of your{" "}
+                                <span className="text-[#85FAD7]">
+                                    reputation
+                                </span>
+                                .
+                            </h1>
+                            <p className="font-semibold text-[#fff] text-lg mb-2 ">
+                                Not the size of your marketing budget.
+                            </p>
+                            <div className="mt-10  flex sm:flex-col gap-[32px]  ">
+                                <Link
+                                    href={
+                                        "https://app.zippycrowd.com/login"
+                                    }
+                                >
+                                    <Button variant="filled">
+                                        Sign up for free
+                                    </Button>
+                                </Link>
                                 <div>
-                                    <h1 className="text-3xl  mb-2 2xl:w-4/5 md:w-11/12 sm:text-2xl font-semibold font-sans text-[#fff] leading-[3rem] ">
-                                        {" "}
-                                        <span className="text-[#85FAD7]">
-                                            Build business{" "}
-                                        </span>
-                                        on the{" "}
-                                        <span className="text-[#85FAD7]">
-                                            strength{" "}
-                                        </span>{" "}
-                                        of your{" "}
-                                        <span className="text-[#a8e6d7]">
-                                            reputation.
-                                        </span>
-                                    </h1>
-                                </div>
-                                <div>
-                                    <p className="font-semibold text-[#fff] text-lg mb-2 ">
-                                        Not the size of your marketing budget.
-                                    </p>
-                                </div>
-                                <div className="mt-10  flex sm:flex-col gap-[32px]  ">
-                                    <Link
-                                        href={
-                                            "https://app.zippycrowd.com/login"
-                                        }
+                                    <Button
+                                        variant="outlined"
+                                        onClick={onClick}
                                     >
-                                        <Button variant="filled">
-                                            Sign up for free
-                                        </Button>
-                                    </Link>
-                                    <div>
-                                        <Button
-                                            variant="outlined"
-                                            onClick={onClick}
-                                        >
-                                            Watch Demo
-                                        </Button>
-                                    </div>
+                                        Watch Demo
+                                    </Button>
                                 </div>
                             </div>
-                            <div className=" w-full p-5 ">
-                                <img
-                                    src={HairIllustration}
-                                    alt="Yoga illustration"
-                                    className=" w-2/3 lg:w-2/4 mx-auto"
-                                />
-                            </div>
+                        </div>
+                        <div className=" w-full p-5 flex justify-end lg:justify-center md:justify-start ">
+                            <img
+                                src={HairIllustration}
+                                alt="Yoga illustration"
+                                className=" w-5/6 lg:w-2/4 md:w-4/6 md:ml-[4.6rem] sm:w-full "
+                            />
                         </div>
                     </div>
                 </Container>
