056814778ce053f54da3dc32f9acfd617855d173
koushith <koushith97@gmail.com>
2023-04-21 08:08:44 +0530

    refactors faq

---
+++ components/homepage/components/faq/answers.tsx
@@ -0,0 +1,134 @@
+import React from "react"
+
+export const answers = {
+    one: (
+        <>
+            <p>
+                ZippyCrowd is a free word-of-mouth platform helping people in
+                small businesses get new customers through personal
+                recommendations.
+            </p>
+            <p className="py-2">
+                We are on a mission to bring more trust into the online world of
+                work.
+            </p>
+        </>
+    ),
+    two: (
+        <p>
+            If you want to reach thousands of people in your customers&apos;
+            communities, and turn them into actual paying clients, you&apos;re
+            going to need a way to get people in your community to talk about
+            you, endorse your work, and easily recommend you on demand.
+            ZippyCrowd makes it easy for you.
+        </p>
+    ),
+    three: (
+        <>
+            <p>
+                Word of mouth is arguably the most important marketing tactic of
+                any business. 92% of people trust recommendations from friends
+                and family over any other type of advertising. In today’s
+                hyper-connected world, a single recommendation from someone you
+                trust will have far greater impact. At ZippyCrowd each
+                recommendation comes from people you know, cutting through the
+                noise, saving time, energy and money, and
+            </p>
+            <span className="font-semibold">
+                creating otherwise unavailable opportunities.
+            </span>
+        </>
+    ),
+    four: (
+        <>
+            <div>
+                ZippyCrowd brings together, into one space, your friends and
+                loyal customers, making it easy for you to find people you need,
+                through people you trust, get your business discovered and your
+                work recommended. Free, direct, with no middleman. The platform
+                is built completely around trust and is based on the notion that
+                people enjoy recommending excellent workmanship and services
+                offered by their friends, families and local communities.
+            </div>
+            <div className="mt-5">
+                With ZippyCrowd you have a single destination for all
+                word-of-mouth recommendations you&apos;ve received from friends
+                and loyal customers
+            </div>
+        </>
+    ),
+    five: (
+        <>
+            {" "}
+            <div>
+                <p>
+                    Let&apos;s face it, reviews of strangers and star ratings
+                    are meaningless — people either recommend you, or they
+                    don&apos;t. ZippyCrowd is a platform that supports and
+                    aggregates recommendations for the good people who do great
+                    work.
+                </p>
+            </div>
+            <div>
+                <ul className="list-disc px-5">
+                    <li>
+                        ZippyCrowd is not collecting review ratings. Instead
+                        ZippyCrowd supports recommendation requests from users
+                    </li>
+                    <li>
+                        Recommendations are allowed only between people in each
+                        other&apos;s trusted crowds
+                    </li>
+                    <li>
+                        Recommendations are done by way of tagging, without the
+                        need for users to write lengthy opinion content. Users
+                        can get more feedback through direct messaging
+                    </li>
+                    <li>
+                        In the absence of recommendation requests (Asks on
+                        ZippyCrowd), users can show support for someone&apos;s
+                        service by clicking on &apos;Claps to support&apos;
+                        their work
+                    </li>
+                    <li>
+                        Trusted crowds&apos; functionality eliminates fake
+                        recommendations
+                    </li>
+                </ul>
+            </div>
+        </>
+    ),
+    six: (
+        <>
+            {" "}
+            <div>
+                ZippyCrowd authenticates everyone&apos;s identity with their
+                Facebook or Google, and facilitates recommendations on demand,
+                only from people you keep in your trusted crowd, and only to
+                people they trust – making recommendations real and trustworthy.
+                The resulting network of connections and trusted crowds exists
+                through only 2 degrees of separation. Each recommendation can
+                only be made by people you have included into your trusted
+                crowd, and they have done the same for you.
+            </div>
+            <div>
+                The emphasis is on real-world relationships – a notion
+                that&apos;s missing from so many social network sites,
+                marketplaces and task platforms.
+            </div>
+        </>
+    ),
+    seven: (
+        <>
+            {" "}
+            ZippyCrowd, in order to keep recommendations ‘authentic’, embedded
+            ‘Trusted Crowd’ feature into the recommendation process, in stark
+            contrast to the review sites saturated with questionable reviews
+            from people you don&apos;t know, Keep your trusted people at your
+            fingertips – friends you can count on, helpful neighbours, great
+            colleagues, your favorite hairdresser, personal trainer, accountant,
+            yoga instructor, designer, physiotherapist, chiropractor, chef,
+            babysitter, dog walker, mechanic, and more!
+        </>
+    ),
+}
--- components/homepage/components/index.ts
+++ components/homepage/components/index.ts
@@ -1,26 +1,3 @@
-// export * from "./common/footer/footer.component"
-// export * from "./common/navbar/navbar.component"
-
-// export * from "./faq/ans.component"
-// export * from "./faq/question.component"
-
-// export * from "./primitive/box/box.component"
-// export * from "./primitive/button/button.component"
-
-// export * from "./primitive/footerlink/footerlink.component"
-// export * from "./primitive/modal/modal.component"
-
-// export * from "./primitive/nav-link/nav-link.component"
-// export * from "./primitive/social-icon/social-icon.component"
-
-// export * from "./primitive/text/text.component"
-// export * from "./primitive/youtube-video/youtube-video.component"
-
-// export * from "./wave/wave-one-svg/wave-one-top.component"
-// export * from "./wave/wave-three-svg/wave-three-bottom.component"
-
-// export * from "./wave/wave-two-svg/wave-two-bottom.component"
-
 export * from "./primitive"
 export * from "./animations/animation"
 
